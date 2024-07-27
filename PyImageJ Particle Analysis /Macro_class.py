20c2571f6cf3594e4e4c78d99fea02434bf6a019
zhongsa <zhong.sa@northeastern.edu>
2024-07-27 16:46:43 -0400

    PyImageJ Application Testing

--- PyImageJ Particle Analysis /Macro_class.py
+++ PyImageJ Particle Analysis /Macro_class.py
@@ -1,37 +1,37 @@
-# creating macro class functions to be called 
-
+'''
+Macro functions to be called in PyImageJ
+'''
 class MacroFunctions:
-    def __init__(self, image_path, results_path, output_path, threshold_min, threshold_max, scale, unit, save_type, pixels):
+    def __init__(self, image_path, results_path, output_path, threshold_min=None, threshold_max=None, scale=None, unit=None, pixels=None):
         #initalize variables 
         self.image_path = image_path
         self.results_path = results_path
         self.output_path = output_path
-        self.threshold_min = int(threshold_min)
-        self.threshold_max = int(threshold_max)
-        self.scale = float(scale)
+        self.threshold_min = threshold_min
+        self.threshold_max = threshold_max
+        self.scale = scale
         self.unit  = unit
-        self.save_type = save_type
-        self.pixels = int(pixels)
+        self.pixels = pixels
 
-    def open_image(image_path): 
+    def open_image(self): 
         '''macro function to open image based on image file path'''
-        open_image_macro = f'''open("{image_path}");'''
+        open_image_macro = f'''open("{self.image_path}");'''
 
         return open_image_macro
     
-    def set_8bit():
+    def set_8bit(self):
         '''macro function to set image to 8bit image type'''
         macro_8bit = f'''run("8-bit");'''
 
         return macro_8bit
     
-    def set_threshold(threshold_min, threshold_max):
-        '''macro function to set image threshold and invert image'''
-        macro_threshold = f'''setThreshold({threshold_min}, {threshold_max});'''
+    def set_threshold(self):
+        '''function to return macto for threshold based on user input'''
+        macro_threshold = f'''setThreshold({self.threshold_min}, {self.threshold_max});'''
 
         return macro_threshold
     
-    def binary_mask():
+    def binary_mask(self):
         '''macro function to apply binary mask onto image'''
         binary_mask = f'''
         run("Invert");
@@ -39,24 +39,25 @@ class MacroFunctions:
 
         return binary_mask
     
-    def set_scale(scale, unit, pixels): 
+    def set_scale(self): 
         '''macro function to implement scale and units'''
-        set_scale = f'''run("Set Scale...", "distance={pixels}, known={scale} unit={unit}");'''
+
+        set_scale = f'''run("Set Scale...", "distance={self.pixels} known={self.scale} unit={self.unit}");'''
 
         return set_scale
     
-    def set_measurements():
+    def set_measurements(self):
         '''macro function to set the measurements for analyzing particles'''
         set_measurements = f'''run("Set Measurements...", "area mean standard min centroid perimeter bounding box fit shape feret's integrated density median skewness kurtosis area_fraction stack position");'''
         
         return set_measurements
     
-    def analyze_particles():
+    def analyze_particles(self):
         '''macro function to analyze particles'''
         analyze_particles = f'''run("Analyze Particles...","size=0-Infinity");'''
 
         return analyze_particles
     
-    def save_type(save_type, results_path):
-        ''' function to save results or preprocessed image'''
-        save_as = f'''saveAs("{save_type}", "{results_path}");'''
+    def save_type(self, save_type, save_path):
+        '''sunction to save results or preprocessed image'''
+        return f'''saveAs("{save_type}", "{save_path}");'''
---
+++ PyImageJ Particle Analysis /PyimageJ_Class.py
@@ -0,0 +1,167 @@
+'''
+PyImageJ Functions to Create Particle CSV file data 
+'''
+from Macro_Class import MacroFunctions
+import cv2 
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import os
+
+class PyImageJApp:
+    def __init__(self, macro_functions):
+        self.macro_functions = macro_functions
+
+    def get_scale(self): 
+        '''function to open OpenCV GUI to allow user to set scale values for ImageJ'''
+
+        # read image into opencv 
+        image = cv2.imread(self.macro_functions.image_path)
+
+        # open OpenCV GUI to allow user to select area 
+        r = cv2.selectROI("Select the area", image, showCrosshair=True, fromCenter=False)
+
+        # close GUI window
+        cv2.destroyWindow("Select the area")
+
+        # get region of interest dimensions(ROI)
+        x, y, w, h = r
+
+        # measure length in pixels
+        scale_in_pixels = w
+
+        #print width in pixels
+        print(f"Width: {scale_in_pixels} pixels")
+
+        scale_in_pixels= int(input('Please type the pixel width value:'))
+
+        return  scale_in_pixels
+    
+    def get_threshold(self):
+        '''function to open interactive Threshold GUI for user to set image Threshold, as user for threshold value'''
+
+        # read image into opencv in grayscale
+        image = cv2.imread(self.macro_functions.image_path, cv2.IMREAD_GRAYSCALE)
+
+        # create plot for threshold and to display image
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(left=0.25, bottom=0.25)
+        image_display = ax.imshow(image, cmap='binary')
+        ax.set_title("Threshold")
+
+        #create slider
+        slider_axes = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+        slider = Slider(slider_axes,
+            label='Threshold Value', 
+            valmin = 1,
+            valmax=255, 
+            orientation='horizontal', 
+            valstep=1
+        )
+
+        # create function to update based on threshold value 
+
+        def update(val):
+            threshold_value = slider.val
+            ret, thresh1 = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
+            image_display.set_data(thresh1)
+            ax.set_title(f"Interactive Threshold - Value: {threshold_value}")
+
+        slider.on_changed(update)
+        plt.show()
+
+        threshold = int(input('Please Type your Threshold Value:'))
+
+        threshold_inverted = 225 - threshold
+
+        return threshold_inverted
+
+    def prompt_user(self):
+        '''function to prompt user for the preprocessing steps they would like to do
+        
+        returns list of operations that user wants to do
+        ''' 
+        # initialize dictionary of operation choices for user to select from 
+        operation_dict = {
+            1:'change image type', 
+            2:'set threshold',
+            3: 'convert to binary mask', 
+            4: 'set scale', 
+            5:'analyze particles',
+            6:'save'
+
+        }
+
+        selected_operations = []
+
+        for key, operation in operation_dict.items():
+            choice = input(f"Do you want to {operation}? (yes/no): ").strip().lower()
+            if choice == 'yes':
+                selected_operations.append(key)
+
+        return selected_operations
+        
+    def running_macros(self):
+        '''function to create macros based on a list of returned operations'''
+        
+        # prompt user to get list of returned operations 
+        operations = self.prompt_user()
+
+        # initialize operation commands to open image and iteratively add onto preprocessing and analysis 
+        macro_cmd = [self.macro_functions.open_image()]
+
+        for operation in operations: 
+            if operation == 1:
+                macro_cmd.append(self.macro_functions.set_8bit())
+
+            elif operation == 2:
+                self.macro_functions.threshold_min = int(self.get_threshold())
+                self.macro_functions.threshold_max = 225 
+                macro_cmd.append(self.macro_functions.set_threshold())
+
+            elif operation == 3: 
+                macro_cmd.append(self.macro_functions.binary_mask())
+
+            elif operation == 4:
+                self.macro_functions.pixels = int(self.get_scale())
+                self.macro_functions.scale = float(input('Input the known scale value:')) 
+                self.macro_functions.unit = input('Please enter the unit for the scale: ')
+                macro_cmd.append(self.macro_functions.set_scale())
+
+            elif operation == 5:
+                macro_cmd.append(self.macro_functions.set_measurements())
+                macro_cmd.append(self.macro_functions.analyze_particles())
+                macro_cmd.append(self.macro_functions.save_type('Results', self.macro_functions.results_path))
+                macro_cmd.append(self.macro_functions.save_type('png',self.macro_functions.output_path))
+
+        # join script
+        macro_script = "\n".join(macro_cmd)
+        return macro_script
+
+
+        
+
+            
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+

6c96a31647c97a3ec40f83d049e00326d35bb25f
dalawson0 <131181282+dalawson0@users.noreply.github.com>
2024-08-22 18:09:39 -0400

    Add new function
    The build_sphericity_metric_dataframe() function was added to generate a new copy of the input dataframe with additional columns for sphericity , cumulative percentage (particle) for sphericity, and cumulative frequency for sphericity diameter.

--- Data-Visualization/sphericity_operations.py
+++ Data-Visualization/sphericity_operations.py
@@ -29,3 +29,27 @@ def add_sphericity( df , area_column_name, perim_column_name ):
     sphericity_ratio = compute_sphericity( df, area_column_name, perim_column_name)
     df["Sphericity"] = sphericity_ratio
     return df
+
+
+
+# Add all appropriate columns related to sphericity metrics to dataframe 
+def build_sphericity_metrics_dataframe( input_dataframe, area_col_name, perimeter_col_name, target_col_name, suf):
+    from frequency_operations import add_cumulative_frequency, add_cumulative_percentage
+    modified_input_dataframe = add_sphericity(
+        df = input_dataframe,
+        area_column_name = area_col_name,
+        perim_column_name= perimeter_col_name)
+    new_df = add_cumulative_frequency(
+         DataFrame= modified_input_dataframe,
+         target_column_name= target_col_name,
+         suffix= suf
+         )
+    new_df = add_cumulative_percentage(
+        DataFrame= new_df,
+        target_column_name= target_col_name,
+        column_suffix= suf
+        )
+    return new_df
+
+## !!!
+# Determine whether it makes sense to create a new dataframe in the add_sphericity column or just in the frequency operation functions!!
